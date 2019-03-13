3e9515e2930f4460811c087a4d4ac21df089fc8d
Tilen Marc <marct15@gmail.com>
2019-03-13 16:07:20 +0100

    Cleaned version with documentation.

--- include/abe/fame.h
+++ include/abe/fame.h
@@ -30,10 +30,11 @@
 
 #include <gmp.h>
 #include <amcl/fp12_BN254.h>
+#include <amcl/ecp_BN254.h>
+#include <amcl/ecp2_BN254.h>
 
-#include "data/vec_curve.h"
 #include "data/vec.h"
-#include "policy.h"
+#include "abe/policy.h"
 #include "internal/errors.h"
 
 /**
@@ -84,8 +85,8 @@ typedef struct cfe_fame_cipher {
 } cfe_fame_cipher;
 
 /**
- * cfe_fame_attrib_keys represents the keys corresponding to owned
- * attributes used for the decryption.
+ * cfe_fame_attrib_keys represents the keys corresponding to
+ * attributes needed for the decryption.
  */
 typedef struct cfe_fame_attrib_keys {
     ECP2_BN254 k0[3];
@@ -95,30 +96,145 @@ typedef struct cfe_fame_attrib_keys {
     size_t num_attrib;
 } cfe_fame_attrib_keys;
 
+/**
+ * Configures a new instance of the scheme.
+ *
+ * @param fame A pointer to an uninitialized struct representing the scheme
+ */
 void cfe_fame_init(cfe_fame *fame);
 
+/**
+ * Frees the memory occupied by the struct members. It does not free
+ * the memory occupied by the struct itself.
+ *
+ * @param fame A pointer to an instance of the scheme (*initialized* cfe_fame
+ * struct)
+ */
+void cfe_fame_free(cfe_fame *fame);
+
+/**
+ * Initializes a secret key.
+ *
+ * @param sk A pointer to an uninitialized struct representing the secret key
+ */
 void cfe_fame_sec_key_init(cfe_fame_sec_key *sk);
 
+/**
+ * Frees the memory occupied by the struct members. It does not free
+ * the memory occupied by the struct itself.
+ *
+ * @param sk A pointer to an instance of the secret key (*initialized*
+ * cfe_fame_sec_key struct)
+ */
+void cfe_fame_sec_key_free(cfe_fame_sec_key *sk);
+
+/**
+ * Generates a master secret key and a public key.
+ *
+ * @param pk A pointer to an initialized cfe_fame_pub_key struct, the public key will be saved here
+ * @param sk A pointer to an initialized cfe_fame_sec_key struct, the secret key will be saved here
+ * @param fame A pointer to an initialized struct representing the scheme
+ */
 void cfe_fame_generate_master_keys(cfe_fame_pub_key *pk,
         cfe_fame_sec_key *sk, cfe_fame *fame);
 
+/**
+ * Initializes the ciphertext that will be generated given a
+ * MSP structure. A MSP structure is needed to decide how much
+ * memory needs to be allocated.
+ *
+ * @param cipher A pointer to an uninitialized struct representing the ciphertext
+ * @param msp A pointer to an initialized struct representing the MSP policy
+ */
 void cfe_fame_cipher_init(cfe_fame_cipher *cipher, cfe_msp *msp);
 
+/**
+ * Frees the memory occupied by the struct members. It does not free
+ * the memory occupied by the struct itself.
+ *
+ * @param cipher A pointer to an instance of the ciphertext (*initialized*
+ * cfe_fame_cipher struct)
+ */
+void cfe_fame_cipher_free(cfe_fame_cipher *cipher);
+
+// TODO: change message to be a string when mapping is defined
+/**
+ * The function takes as an input a message, a MSP structure describing a
+ * policy which attributes are needed for the decryption, and a public
+ * key. It creates an encryption of the message.
+ *
+ * @param cipher A pointer to an initialized cfe_fame_cipher struct, the
+ * encryption will be saved here
+ * @param msg An element of FP12_BN254 representing the message
+ * @param msp A pointer to an initialized struct representing the MSP policy
+ * @param pk A pointer to an initialized struct representing the public key
+ * @param fame A pointer to an initialized struct representing the scheme
+ */
 void cfe_fame_encrypt(cfe_fame_cipher *cipher, FP12_BN254 *msg,
         cfe_msp *msp, cfe_fame_pub_key *pk, cfe_fame *fame);
 
+/**
+ * Initializes attribute keys needed for the decryption. The number of attributes
+ * needs to be specified to decide how much memory needs to be allocated.
+ *
+ * @param keys A pointer to an uninitialized struct representing the keys
+ * @param num_attrib The number of attributes that the keys will contain
+ */
 void cfe_fame_attrib_keys_init(cfe_fame_attrib_keys *keys, size_t num_attrib);
 
+/**
+ * Frees the memory occupied by the struct members. It does not free
+ * the memory occupied by the struct itself.
+ *
+ * @param keys A pointer to an instance of attribute keys (*initialized*
+ * cfe_fame_attrib_keys struct)
+ */
+void cfe_fame_attrib_keys_free(cfe_fame_attrib_keys *keys);
+
+/**
+ * The function given an array of attributes and master secret keys creates
+ * keys corresponding to attributes that can be used for the decryption.
+ *
+ * @param keys A pointer to an initialized cfe_fame_attrib_keys struct,
+ * the keys for the decryption will be saved here
+ * @param gamma An array of attributes for which keys will be generated
+ * @param num_attrib Length of the array gamma, i.e. the number of attributes
+ * @param sk A pointer to a struct representing the secret keys
+ * @param fame A pointer to an initialized struct representing the scheme
+ */
 void cfe_fame_generate_attrib_keys(cfe_fame_attrib_keys *keys, int *gamma,
         size_t num_attrib, cfe_fame_sec_key *sk, cfe_fame *fame);
 
+// TODO: change decryption to be a string when mapping is defined
+/**
+ * The function takes as an input a cipher and keys and tries to decrypt
+ * the cipher. If the keys were properly generated, this is possible if and only
+ * if the rows of the MSP matrix corresponding to owned attributes span the
+ * vector [1, 0,..., 0]. If this is not possible, i.e. keys are insufficient,
+ * the function returns the corresponding error.
+ *
+ * @param res An element of FP12_BN254, the decryption will be saved here
+ * @param cipher A pointer to an initialized struct representing the ciphertext
+ * @param keys A pointer to an initialized struct representing the keys for the decryption
+ * @param fame A pointer to an initialized struct representing the scheme
+ * @return Error code
+ */
 cfe_error cfe_fame_decrypt(FP12_BN254 *res, cfe_fame_cipher *cipher,
                            cfe_fame_attrib_keys *keys, cfe_fame *fame);
 
-char *strings_concat_for_hash(char *str, ...);
+/**
+ * A helping function used in cfe_fame_encrypt and cfe_fame_generate_attrib_keys.
+ */
+char *cfe_strings_concat_for_hash(char *str, ...);
 
-char *int_to_str(int i);
+/**
+ * A helping function used in cfe_fame_encrypt and cfe_fame_generate_attrib_keys.
+ */
+char *cfe_int_to_str(int i);
 
-void hash_G1(ECP_BN254 *g, char *str);
+/**
+ * A helping function used in cfe_fame_encrypt and cfe_fame_generate_attrib_keys.
+ */
+void cfe_hash_G1(ECP_BN254 *g, char *str);
 
 #endif
--- src/abe/fame.c
+++ src/abe/fame.c
@@ -29,20 +29,22 @@
 #include <stdarg.h>
 #include <amcl/big_256_56.h>
 #include <amcl/pair_BN254.h>
-#include <memory.h>
 #include <math.h>
+#include <string.h>
 
 #include "internal/common.h"
 #include "internal/big.h"
 #include "sample/uniform.h"
-#include "abe/policy.h"
 #include "abe/fame.h"
-#include "data/vec_curve.h"
 
 
 void cfe_fame_init(cfe_fame *fame) {
     mpz_init(fame->p);
     mpz_from_BIG_256_56(fame->p, (int64_t *) CURVE_Order_BN254);
+}
+
+void cfe_fame_free(cfe_fame *fame) {
+    mpz_clear(fame->p);
 }
 
 void cfe_fame_sec_key_init(cfe_fame_sec_key *sk) {
@@ -51,30 +53,43 @@ void cfe_fame_sec_key_init(cfe_fame_sec_key *sk) {
     }
 }
 
+void cfe_fame_sec_key_free(cfe_fame_sec_key *sk) {
+    for (size_t i = 0; i < 4; i++) {
+        mpz_clear(sk->part_int[i]);
+    }
+}
+
 void cfe_fame_generate_master_keys(cfe_fame_pub_key *pk, cfe_fame_sec_key *sk, cfe_fame *fame) {
+    // prepare variables
     BIG_256_56 tmp_big;
     mpz_t tmp;
     mpz_init(tmp);
     cfe_vec val;
     cfe_vec_init(&val, 7);
+
+    // sample randomness
     cfe_uniform_sample_vec(&val, fame->p);
 
+    // set int part of the secret key
     for (size_t i = 0; i < 4; i++) {
         mpz_set(sk->part_int[i], val.vec[i]);
     }
 
+    // set the elliptic curve part of the secret key
     for (size_t i = 0; i < 3; i++) {
         BIG_256_56_from_mpz(tmp_big, val.vec[i + 4]);
         ECP_BN254_generator(&(sk->part_G1[i]));
         ECP_BN254_mul(&(sk->part_G1[i]), tmp_big);
     }
 
+    // set the elliptic curve part of the public key
     for (size_t i = 0; i < 2; i++) {
         BIG_256_56_from_mpz(tmp_big, val.vec[i]);
         ECP2_BN254_generator(&(pk->part_G2[i]));
         ECP2_BN254_mul(&(pk->part_G2[i]), tmp_big);
     }
 
+    // set the field part of the public key
     ECP_BN254 gen1;
     ECP2_BN254 gen2;
     FP12_BN254 pair;
@@ -100,21 +115,32 @@ void cfe_fame_cipher_init(cfe_fame_cipher *cipher, cfe_msp *msp) {
     cipher->msp.row_to_attrib = (int *) cfe_malloc(msp->mat.rows * (sizeof(int)));
 }
 
+void cfe_fame_cipher_free(cfe_fame_cipher *cipher) {
+    free(cipher->ct);
+    cfe_mat_free(&(cipher->msp.mat));
+    free(cipher->msp.row_to_attrib);
+}
+
 void cfe_fame_encrypt(cfe_fame_cipher *cipher, FP12_BN254 *msg, cfe_msp *msp, cfe_fame_pub_key *pk, cfe_fame *fame) {
-    cfe_vec s;
+    // prepare the variables
+    cfe_vec s, msp_row;
     cfe_vec_init(&s, 2);
+    cfe_vec_init(&msp_row, msp->mat.cols);
+    mpz_t tmp;
+    mpz_init(tmp);
+    BIG_256_56 tmp_big;
+    char *for_hash, *str_attrib, *str_l, *str_k, *str_j;
+    ECP_BN254 hs;
+    ECP_BN254 hs_pow_m;
+
+    // sample the randomness
     cfe_uniform_sample_vec(&s, fame->p);
     BIG_256_56 s_big[2];
     for (size_t i = 0; i < 2; i++) {
         BIG_256_56_from_mpz(s_big[i], s.vec[i]);
     }
 
-    mpz_t tmp;
-    mpz_init(tmp);
-    BIG_256_56 tmp_big;
-    ECP2_BN254 gen2;
-    ECP2_BN254_generator(&gen2);
-
+    // calculate ciphertext
     for (size_t i = 0; i < 2; i++) {
         ECP2_BN254_copy(&(cipher->ct0[i]), &(pk->part_G2[i]));
         ECP2_BN254_mul(&(cipher->ct0[i]), s_big[i]);
@@ -124,55 +150,49 @@ void cfe_fame_encrypt(cfe_fame_cipher *cipher, FP12_BN254 *msg, cfe_msp *msp, cf
     ECP2_BN254_generator(&(cipher->ct0[2]));
     ECP2_BN254_mul(&(cipher->ct0[2]), tmp_big);
 
-    char *for_hash, *str_attrib, *str_l, *str_k, *str_j;
-    ECP_BN254 hs[2];
-    ECP_BN254 hs_to_m;
-    cfe_vec msp_row;
-    cfe_vec_init(&msp_row, msp->mat.cols);
     for (size_t i = 0; i < msp->mat.rows; i++) {
         cipher->msp.row_to_attrib[i] = msp->row_to_attrib[i];
         cfe_mat_get_row(&msp_row, &(msp->mat), i);
         cfe_mat_set_vec(&(cipher->msp.mat), &msp_row, i);
 
-        str_attrib = int_to_str(msp->row_to_attrib[i]);
+        str_attrib = cfe_int_to_str(msp->row_to_attrib[i]);
         for (int l = 0; l < 3; l++) {
-            str_l = int_to_str(l);
+            str_l = cfe_int_to_str(l);
+            ECP_BN254_inf(&(cipher->ct[i][l]));
             for (int k = 0; k < 2; k++) {
-                str_k = int_to_str(k);
-                for_hash = strings_concat_for_hash(str_attrib, " ", str_l, " ", str_k, NULL);
-                hash_G1(&(hs[k]), for_hash);
-                ECP_BN254_mul(&(hs[k]), s_big[k]);
+                str_k = cfe_int_to_str(k);
+                for_hash = cfe_strings_concat_for_hash(str_attrib, " ", str_l, " ", str_k, NULL);
+                cfe_hash_G1(&hs, for_hash);
+                ECP_BN254_mul(&hs, s_big[k]);
+                ECP_BN254_add(&(cipher->ct[i][l]), &hs);
                 free(str_k);
                 free(for_hash);
             }
 
-            ECP_BN254_copy(&(cipher->ct[i][l]), &(hs[0]));
-            ECP_BN254_add(&(cipher->ct[i][l]), &(hs[1]));
-
             for (int j = 0; j < (int) msp->mat.cols; j++) {
-                str_j = int_to_str(j);
+                str_j = cfe_int_to_str(j);
+                ECP_BN254_inf(&hs_pow_m);
                 for (int k = 0; k < 2; k++) {
-                    str_k = int_to_str(k);
-                    for_hash = strings_concat_for_hash((char *) "0 ", str_j, " ", str_l, " ", str_k, NULL);
-                    hash_G1(&(hs[k]), for_hash);
-                    ECP_BN254_mul(&(hs[k]), s_big[k]);
+                    str_k = cfe_int_to_str(k);
+                    for_hash = cfe_strings_concat_for_hash((char *) "0 ", str_j, " ", str_l, " ", str_k, NULL);
+                    cfe_hash_G1(&(hs), for_hash);
+                    ECP_BN254_mul(&(hs), s_big[k]);
+                    ECP_BN254_add(&hs_pow_m, &(hs));
                     free(str_k);
                     free(for_hash);
                 }
                 free(str_j);
 
-                ECP_BN254_copy(&hs_to_m, &(hs[0]));
-                ECP_BN254_add(&hs_to_m, &(hs[1]));
                 if (mpz_sgn(msp->mat.mat[i].vec[j]) == -1) {
                     mpz_neg(tmp, msp->mat.mat[i].vec[j]);
                     BIG_256_56_from_mpz(tmp_big, tmp);
-                    ECP_BN254_mul(&hs_to_m, tmp_big);
-                    ECP_BN254_neg(&hs_to_m);
+                    ECP_BN254_mul(&hs_pow_m, tmp_big);
+                    ECP_BN254_neg(&hs_pow_m);
                 } else {
                     BIG_256_56_from_mpz(tmp_big, msp->mat.mat[i].vec[j]);
-                    ECP_BN254_mul(&hs_to_m, tmp_big);
+                    ECP_BN254_mul(&hs_pow_m, tmp_big);
                 }
-                ECP_BN254_add(&(cipher->ct[i][l]), &hs_to_m);
+                ECP_BN254_add(&(cipher->ct[i][l]), &hs_pow_m);
             }
             free(str_l);
         }
@@ -184,65 +204,9 @@ void cfe_fame_encrypt(cfe_fame_cipher *cipher, FP12_BN254 *msg, cfe_msp *msp, cf
     FP12_BN254_mul(&(cipher->ct_prime), &tmp_GT);
     FP12_BN254_mul(&(cipher->ct_prime), msg);
 
+    // clear up
+    cfe_vec_frees(&s, &msp_row, NULL);
     mpz_clear(tmp);
-}
-
-void hash_G1(ECP_BN254 *g, char *str) {
-    octet tmp;
-    tmp.val = str;
-    ECP_BN254_mapit(g, &tmp);
-}
-
-char *strings_concat_for_hash(char *start, ...) {
-    // sum the length of all the strings
-    va_list ap;
-    size_t len = 0;
-    va_start(ap, start);
-    char *str = start;
-    while (str != NULL) {
-        len += strlen(str);
-        str = va_arg(ap, char*);
-    }
-    va_end(ap);
-    char *res = (char *) cfe_malloc((MODBYTES_256_56 + 1) * sizeof(char));
-
-    // set the string
-    va_start(ap, start);
-    str = start;
-    size_t j = 0;
-    while (str != NULL) {
-        for (size_t i = 0; i < strlen(str); i++) {
-            res[j] = str[i];
-            j++;
-        }
-        str = va_arg(ap, char*);
-    }
-    for (size_t i = j; i < MODBYTES_256_56 + 1; i++) {
-        res[i] = '\0';
-    }
-
-    va_end(ap);
-
-    return res;
-}
-
-char *int_to_str(int i) {
-    int len;
-    if (i == 0) {
-        len = 1;
-    } else {
-        len = (int) log10(i) + 1;
-    }
-    char *result = (char *) cfe_malloc((len + 1) * sizeof(char));
-
-    for (int j = 0; j < len; j++) {
-        result[len - j - 1] = '0' + (i % 10);
-        i = i / 10;
-    }
-
-    result[len] = '\0';
-
-    return result;
 }
 
 void cfe_fame_attrib_keys_init(cfe_fame_attrib_keys *keys, size_t num_attrib) {
@@ -251,17 +215,28 @@ void cfe_fame_attrib_keys_init(cfe_fame_attrib_keys *keys, size_t num_attrib) {
     keys->num_attrib = num_attrib;
 }
 
+void cfe_fame_attrib_keys_free(cfe_fame_attrib_keys *keys) {
+    free(keys->k);
+    free(keys->row_to_attrib);
+}
+
 void cfe_fame_generate_attrib_keys(cfe_fame_attrib_keys *keys, int *gamma,
         size_t num_attrib, cfe_fame_sec_key *sk, cfe_fame *fame) {
+    // prepare variables
     cfe_vec r, sigma;
     cfe_vec_init(&r, 2);
     cfe_vec_init(&sigma, num_attrib);
-    cfe_uniform_sample_vec(&r, fame->p);
-    cfe_uniform_sample_vec(&sigma, fame->p);
-
     BIG_256_56 tmp_big, a_inv_big[2];
     mpz_t pow[3], a_inv[2], sigma_prime;
     mpz_inits(pow[0], pow[1], pow[2], a_inv[0], a_inv[1], sigma_prime, NULL);
+    ECP_BN254 g_sigma, hs, g_sigma_prime;
+    char *str_attrib, *str_t, *str_j, *for_hash;
+
+    // sample randomness
+    cfe_uniform_sample_vec(&r, fame->p);
+    cfe_uniform_sample_vec(&sigma, fame->p);
+
+    // prepare values
     for (size_t j = 0; j < 2; j++) {
         mpz_mul(pow[j], sk->part_int[2 + j], r.vec[j]);
         mpz_mod(pow[j], pow[j], fame->p);
@@ -271,43 +246,45 @@ void cfe_fame_generate_attrib_keys(cfe_fame_attrib_keys *keys, int *gamma,
     mpz_add(pow[2], r.vec[0], r.vec[1]);
     mpz_mod(pow[2], pow[2], fame->p);
 
+    // calculate keys
     for (size_t j = 0; j < 3; j++) {
         BIG_256_56_from_mpz(tmp_big, pow[j]);
         ECP2_BN254_generator(&(keys->k0[j]));
         ECP2_BN254_mul(&(keys->k0[j]), tmp_big);
     }
 
-    ECP_BN254 g_sigma, hs, g_sigma_prime;
-    char *str_attrib, *str_t, *str_j, *for_hash;
     for (size_t i = 0; i < num_attrib; i++) {
         ECP_BN254_generator(&g_sigma);
         BIG_256_56_from_mpz(tmp_big, sigma.vec[i]);
         ECP_BN254_mul(&g_sigma, tmp_big);
 
-        str_attrib = int_to_str(gamma[i]);
-
+        str_attrib = cfe_int_to_str(gamma[i]);
         for (int t = 0; t < 2; t++) {
-            str_t = int_to_str(t);
+            str_t = cfe_int_to_str(t);
 
             ECP_BN254_copy(&(keys->k[i][t]), &g_sigma);
             for (int j = 0; j < 3; j++) {
-                str_j = int_to_str(j);
-
-                for_hash = strings_concat_for_hash(str_attrib, " ", str_j, " ", str_t, NULL);
-                hash_G1(&hs, for_hash);
+                str_j = cfe_int_to_str(j);
+                for_hash = cfe_strings_concat_for_hash(str_attrib, " ", str_j, " ", str_t, NULL);
+                cfe_hash_G1(&hs, for_hash);
 
                 BIG_256_56_from_mpz(tmp_big, pow[j]);
                 ECP_BN254_mul(&hs, tmp_big);
                 ECP_BN254_add(&(keys->k[i][t]), &hs);
-            }
 
+                free(str_j);
+                free(for_hash);
+            }
             ECP_BN254_mul(&(keys->k[i][t]), a_inv_big[t]);
+
+            free(str_t);
         }
 
         ECP_BN254_copy(&(keys->k[i][2]), &g_sigma);
         ECP_BN254_neg(&(keys->k[i][2]));
-
         keys->row_to_attrib[i] = gamma[i];
+
+        free(str_attrib);
     }
 
     cfe_uniform_sample(sigma_prime, fame->p);
@@ -317,27 +294,36 @@ void cfe_fame_generate_attrib_keys(cfe_fame_attrib_keys *keys, int *gamma,
 
     for (int t = 0; t < 2; t++) {
         ECP_BN254_copy(&(keys->k_prime[t]), &g_sigma_prime);
-        str_t = int_to_str(t);
+        str_t = cfe_int_to_str(t);
         for (int j = 0; j < 3; j++) {
-            str_j = int_to_str(j);
-            for_hash = strings_concat_for_hash((char *) "0 0 ", str_j, " ", str_t, NULL);
-            hash_G1(&hs, for_hash);
+            str_j = cfe_int_to_str(j);
+            for_hash = cfe_strings_concat_for_hash((char *) "0 0 ", str_j, " ", str_t, NULL);
+            cfe_hash_G1(&hs, for_hash);
 
             BIG_256_56_from_mpz(tmp_big, pow[j]);
             ECP_BN254_mul(&hs, tmp_big);
             ECP_BN254_add(&(keys->k_prime[t]), &hs);
+
+            free(str_j);
+            free(for_hash);
         }
         ECP_BN254_mul(&(keys->k_prime[t]), a_inv_big[t]);
         ECP_BN254_add(&(keys->k_prime[t]), &(sk->part_G1[t]));
+
+        free(str_t);
     }
     ECP_BN254_copy(&(keys->k_prime[2]), &g_sigma_prime);
     ECP_BN254_neg(&(keys->k_prime[2]));
     ECP_BN254_add(&(keys->k_prime[2]), &(sk->part_G1[2]));
+
+    // clear up
+    mpz_clears(pow[0], pow[1], pow[2], a_inv[0], a_inv[1], sigma_prime, NULL);
+    cfe_vec_frees(&sigma, &r, NULL);
 }
 
 cfe_error cfe_fame_decrypt(FP12_BN254 *res, cfe_fame_cipher *cipher,
         cfe_fame_attrib_keys *keys, cfe_fame *fame) {
-
+    // determine the intersection between owned attributes and specified in encryption
     size_t count_attrib = 0;
     size_t positions_keys[keys->num_attrib];
     size_t positions_msp[keys->num_attrib];
@@ -352,38 +338,36 @@ cfe_error cfe_fame_decrypt(FP12_BN254 *res, cfe_fame_cipher *cipher,
         }
     }
 
+    // prepare all the variables
     cfe_mat mat_for_keys, mat_for_keys_trans;
     cfe_mat_init(&mat_for_keys, count_attrib, cipher->msp.mat.cols);
     cfe_mat_init(&mat_for_keys_trans, cipher->msp.mat.cols, count_attrib);
-    cfe_vec tmp;
+    cfe_vec tmp, one_vec, alpha;
     cfe_vec_init(&tmp, cipher->msp.mat.cols);
+    mpz_t zero;
+    mpz_init_set_si(zero, 0);
+    ECP_BN254 ct_prod[3], key_prod[3], x_pow_alpha;
+    BIG_256_56 alpha_big;
+    FP12_BN254 ct_pairing, key_pairing, key_pairing_inv;
+
+
+    // determine needed attributes
     for (size_t i = 0; i < count_attrib; i++) {
         cfe_mat_get_row(&tmp, &(cipher->msp.mat), positions_msp[i]);
         cfe_mat_set_vec(&mat_for_keys, &tmp, i);
     }
     cfe_mat_transpose(&mat_for_keys_trans, &mat_for_keys);
-
-    cfe_vec one_vec, alpha;
-    mpz_t zero;
-    mpz_init_set_si(zero, 0);
     cfe_vec_init(&one_vec, cipher->msp.mat.cols);
     cfe_vec_set_const(&one_vec, zero);
     mpz_set_ui(one_vec.vec[0], 1);
 
-
     cfe_error check = gaussian_elimination(&alpha, &mat_for_keys_trans, &one_vec, fame->p);
-    cfe_mat_free(&mat_for_keys_trans);
-    cfe_vec_free(&one_vec);
-    mpz_clear(zero);
     if (check) {
         return CFE_ERR_INSUFFICIENT_KEYS;
     }
 
+    // calculate decryption
     FP12_BN254_copy(res, &(cipher->ct_prime));
-
-    ECP_BN254 ct_prod[3], key_prod[3], x_pow_alpha;
-    BIG_256_56 alpha_big;
-    FP12_BN254 ct_pairing, key_pairing, key_pairing_inv;
     for (size_t j = 0; j < 3; j++) {
         ECP_BN254_inf(&(ct_prod[j]));
         ECP_BN254_inf(&(key_prod[j])); //check inf
@@ -410,5 +394,72 @@ cfe_error cfe_fame_decrypt(FP12_BN254 *res, cfe_fame_cipher *cipher,
         FP12_BN254_mul(res, &key_pairing_inv);
     }
 
+    // clear up
+    cfe_mat_frees(&mat_for_keys_trans, &mat_for_keys, NULL);
+    cfe_vec_frees(&one_vec, &alpha, &tmp, NULL);
+    mpz_clear(zero);
+
     return CFE_ERR_NONE;
+}
+
+// cfe_hash_G1 hashes a string of length MODBYTES_256_56
+// into the elliptic group represented by ECP_BN254.
+void cfe_hash_G1(ECP_BN254 *g, char *str) {
+    octet tmp;
+    tmp.val = str;
+    ECP_BN254_mapit(g, &tmp);
+}
+
+// cfe_strings_concat_for_hash joins the given strings to create
+// a string of length MODBYTES_256_56 needed for the hashing function.
+char *cfe_strings_concat_for_hash(char *start, ...) {
+    // sum the length of all the strings
+    va_list ap;
+    size_t len = 0;
+    va_start(ap, start);
+    char *str = start;
+    while (str != NULL) {
+        len += strlen(str);
+        str = va_arg(ap, char*);
+    }
+    va_end(ap);
+    char *res = (char *) cfe_malloc((MODBYTES_256_56 + 1) * sizeof(char));
+
+    // set the string
+    va_start(ap, start);
+    str = start;
+    size_t j = 0;
+    while (str != NULL) {
+        for (size_t i = 0; i < strlen(str); i++) {
+            res[j] = str[i];
+            j++;
+        }
+        str = va_arg(ap, char*);
+    }
+    for (size_t i = j; i < MODBYTES_256_56 + 1; i++) {
+        res[i] = '\0';
+    }
+    va_end(ap);
+
+    return res;
+}
+
+// cfe_int_to_str changes a non-negative int into a string of its
+// decimal representation
+char *cfe_int_to_str(int i) {
+    int len;
+    if (i == 0) {
+        len = 1;
+    } else {
+        len = (int) log10(i) + 1;
+    }
+    char *result = (char *) cfe_malloc((len + 1) * sizeof(char));
+
+    for (int j = 0; j < len; j++) {
+        result[len - j - 1] = '0' + (i % 10);
+        i = i / 10;
+    }
+    result[len] = '\0';
+
+    return result;
 }
