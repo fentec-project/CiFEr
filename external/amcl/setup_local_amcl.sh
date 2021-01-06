4afc712078faf741eca8a3e93c83a6d233c1b16b
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-01-06 15:45:09 +0100

    Support AMCL with a submodule

---
+++ external/amcl/setup_local_amcl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+VENDOR_PATH=vendor/amcl/version3/c
+
+# Fetch source
+git submodule update
+
+# Compile
+(
+  cd $VENDOR_PATH ;
+  rm * ;
+  git checkout . ;
+  sed -ie 's/ -O3/ -O3 -fPIC/g' config64.py ;
+  echo -e "18\n0" | python config64.py
+)
+
+# "Install"
+cp $VENDOR_PATH/*.h $VENDOR_PATH/amcl.a external/amcl
+mv external/amcl/amcl.a external/amcl/libamcl.a
+
+# Clean up
+(
+  cd $VENDOR_PATH ;
+  rm * ;
+  git checkout . ;
+)
